803d8171122406bb9f17c7c0ac7476afc5fa1de5
Joe <josephagrane@gmail.com>
2023-06-27 09:00:45 +0100

    added a live data collector class

--- sim_data_collection/data_collector.py
+++ sim_data_collection/data_collector.py
@@ -1,6 +1,20 @@
-def main():
-    print('Hi from sim_data_collection.')
+import rclpy
+from sim_data_collection.live_data_collector import LiveDataCollector as Node
 
+logger = None
+
+def callback(msg): 
+    logger.info(str(type(msg)))
+
+def main():
+    global logger
+    rclpy.init()
+    instance = Node()
+    instance.register_callback("drive_request", callback)
+    instance.register_callback("car_request", callback)
+    logger = instance.get_logger()
+    rclpy.spin(instance)
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
---
+++ sim_data_collection/live_data_collector.py
@@ -0,0 +1,67 @@
+from rclpy.node import Node as RosNode
+import ugrdv_msgs.msg as ugrdv_msgs
+import eufs_msgs.msg as eufs_msgs
+from rclpy.qos import QoSProfile
+
+class LiveDataCollector(RosNode):
+    def __init__(self):
+        super().__init__("data_collector")
+        self.get_logger().info("data_collector starting...") 
+        self._init_qos()
+        self._init_message_info()
+        self._init_params()
+        self._init_subscriptions()
+
+    def _init_qos(self):
+        self._qos_profile = 1#QoSProfile()
+
+    def _init_message_info(self):
+        def _message_description(topic, type):
+            d = {
+                "topic" : topic,
+                "type" : type,
+                "callbacks" : [],
+                "subscription" : None
+            }
+            return d
+        self._messages = {
+            "drive_request" : _message_description("/ugrdv/drive_request", ugrdv_msgs.DriveRequest),
+            "car_request" : _message_description("/ugrdv/car_request", ugrdv_msgs.CarRequest),
+            "path_planning_path_velocity_request" : _message_description("/ugrdv/path_velocity_request", ugrdv_msgs.PathVelocityRequest),
+            "mission_path_velocity_request" : _message_description("/ugrdv/mission_path_velocity_request", ugrdv_msgs.PathVelocityRequest),
+            "perception_cones" : _message_description("/ugrdv/perception/simulated_cones", ugrdv_msgs.Cone3dArray),
+            "ground_truth_cones" : _message_description("/ground_truth/cones", eufs_msgs.ConeArrayWithCovariance),
+            "ground_truth_state" : _message_description("/ground_truth/state", eufs_msgs.CarState),
+        }
+
+    def _init_params(self):
+        # use the topic names in self._messages as a default
+        self._param_names_and_values = {
+            f"{id}_topic" : data["topic"] for (id,data) in self._messages.items()
+        }
+
+        # declare through ros
+        for (name, default) in self._param_names_and_values.items():
+            self.declare_parameter(name, default)
+        # get values (or default if none provided)
+        for name in self._param_names_and_values.keys():
+            self._param_names_and_values[name] = self.get_parameter(name).value 
+
+    def _init_subscriptions(self):
+        def _make_callback(id):
+            return lambda msg : self._fire_callbacks(id, msg)
+        for (id, data) in self._messages.items():
+            data["subscription"] = self.create_subscription(
+                data["type"],
+                data["topic"],
+                _make_callback(id) ,
+                self._qos_profile
+            )
+
+    def _fire_callbacks(self, id, msg):
+        assert id in self._messages
+        for fn in self._messages[id]["callbacks"]: fn(msg)
+
+    def register_callback(self, id, fn):
+        assert id in self._messages
+        self._messages[id]["callbacks"].append(fn)
