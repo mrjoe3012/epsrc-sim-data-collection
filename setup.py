0143f61eb15b3c6c518bc4baa4341dd82d1a5867
Joe <josephagrane@gmail.com>
2023-06-27 07:51:14 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'sim_data_collection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Joseph Agrane',
+    maintainer_email='josephagrane@gmail.com',
+    description='This package serializes ROS messages from simulation runs into a database in which the relationships between emssages are directly exposed.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'data_collector = sim_data_collection.data_collector:main'
+        ],
+    },
+)
---
+++ sim_data_collection/data_collector.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from sim_data_collection.')
+
+
+if __name__ == '__main__':
+    main()
