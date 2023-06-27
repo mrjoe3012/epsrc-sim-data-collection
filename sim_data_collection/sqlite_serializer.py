3cfcb8bd339c20350374e1c55bea61171a04cb82
Joe <josephagrane@gmail.com>
2023-06-27 09:48:11 +0100

    added framework for message serialization

--- sim_data_collection/sqlite_serializer.py
+++ sim_data_collection/sqlite_serializer.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3 as db
 
 class SQLiteSerializer:
     def __init__(self):
@@ -6,14 +6,47 @@ class SQLiteSerializer:
 
     def open(self, path):
         assert self._open == False
+        self._connection = db.connect(path)
         self._open = True
 
     def close(self):
         assert self._open == True
+        self._connection.close()
         self._open = False
 
     def create_new_database(self):
         assert self._open == True
+        cursor = self._connection.cursor()
+        queries = ("""
+        CREATE TABLE vcu_status(
+            hash VARCHAR(32) PRIMARY KEY,
+            timestamp INT NOT NULL,
+            data BLOB
+        );
+        """,
+        """
+        CREATE TABLE perception_cones(
+            hash VARCHAR(32) PRIMARY KEY,
+            timestamp INT NOT NULL,
+            data BLOB
+        );
+        """,
+        """
+        CREATE TABLE ground_truth_cones(
+            hash VARCHAR(32) PRIMARY KEY,
+            timestamp INT NOT NULL,
+            data BLOB
+        );
+        """,
+        """
+        CREATE TABLE ground_truth_state(
+            hash VARCHAR(32) PRIMARY KEY,
+            timestamp INT NOT NULL,
+            data BLOB
+        );
+        """,
+        )
+        for sql in queries: cursor.execute(sql)
 
     def serialize_message(self, msg):
         pass
---
+++ sim_data_collection/utils.py
@@ -0,0 +1,25 @@
+from rclpy.serialization import serialize_message
+import hashlib, copy
+
+def getMessageHash(msg):
+	"""
+	Returns the MD5 hash for a message.
+	
+	:param msg: The message to calculate a hash for. Must have a ugrdv_msgs/Meta field named meta.
+	:returns: A hex string containing the hash.
+	"""
+	serialized = serialize_message(msg)
+	hash = hashlib.md5(serialized, usedforsecurity=False)
+	return hash.hexdigest().upper()
+
+def getMessageHashMeta(msg):
+    """
+    Returns the MD5 hash for a message with a meta field.
+
+    :param msg: The message to calculate a hash for. Must have a ugrdv_msgs/Meta field named meta.
+    :returns: A hex string containing the hash.
+    """
+    msg = copy.deepcopy(msg)
+    msg.meta.hash = ""
+    hash = getMessageHash(msg)
+    return hash
